c1b29242e4b0dc5bd1dd4d0061c1d8450fa94006
Ubaid Ullah <ubaidullah4211@gmail.com>
2023-06-01 22:44:22 +0500

    adding ec2 hardhat node

--- client/src/App.js
+++ client/src/App.js
@@ -20,10 +20,15 @@ function App() {
   useEffect(() => {
     const init = async () => {
       const provider = await new ethers.providers.Web3Provider(window.ethereum)
+      console.log("provider = ",provider)
+
+      console.log("window.ethereum = ",window.ethereum)
+
       setProvider(provider)
+      console.log("provider after setProvider(provider) = ",provider)
 
       const contract = await new ethers.Contract(
-        '0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0',
+        '0x5FbDB2315678afecb367f032d93F642f64180aa3',
         artifact.abi
       )
       setContract(contract)
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,17 @@
 require("@nomiclabs/hardhat-waffle")
+require('dotenv').config({path:__dirname+'/.env'});
+const { API_URL} = process.env;
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  networks: {
+    development: {
+      url: 'http://localhost:8545', // Change to your local development network URL
+    },
+    aws: {
+      url: API_URL, // Set your AWS EC2 node URL in the .env file
+    },
+  },
   solidity: {
     version: "0.8.0"
   },
